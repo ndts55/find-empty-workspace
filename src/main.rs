bb90dbf885c48bd5e7deff77e470b484ec7c8c87
Nicolas Torres <nicolastorres@mailbox.org>
2022-02-24 14:55:30 +0100

    changed focus flag to stay (inverse) and changed default behavior to always switch workspace focus

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,20 @@ struct Opt {
     )]
     move_container: bool,
 
-    #[structopt(short = "f", long = "focus", help = "Focus an empty workspace.")]
-    focus_workspace: bool,
+    #[structopt(short, long, help = "Stay on the current workspace.")]
+    stay: bool,
 
-    #[structopt(name = "NAMES", required = true, min_values = 1)]
+    #[structopt(
+        name = "Workspace Names",
+        required = true,
+        min_values = 1,
+        help = "Names of all possible workspaces."
+    )]
     names: Vec<String>,
 }
 
 fn main() -> io::Result<()> {
+    // Run the actual logic and notify the user about any errors.
     if let Err(error_str) = run() {
         Command::new("notify-send")
             .arg(format!("{0}", error_str))
@@ -34,9 +40,6 @@ fn main() -> io::Result<()> {
 
 fn run() -> Result<(), &'static str> {
     let opt = Opt::from_args();
-    if !opt.move_container && !opt.focus_workspace {
-        return Ok(());
-    }
 
     let mut i3 = I3::connect().map_err(|_| "Unable to connect to i3")?;
     let active_workspaces = i3
@@ -45,24 +48,9 @@ fn run() -> Result<(), &'static str> {
 
     is_action_necessary(&mut i3, &active_workspaces)?;
 
-    let active_names = active_workspaces.into_iter().map(|ws| ws.name).collect();
-    let free_workspace_name = find_next_free_workspace(&opt.names, &active_names)
-        .ok_or("No empty workspaces available.")?;
-
-    let mut command = String::new();
-
-    if opt.move_container {
-        let move_command = format!("move container to workspace {0};", free_workspace_name);
-        command.push_str(&move_command);
-    }
-
-    if opt.focus_workspace {
-        let focus_command = format!("workspace {0};", free_workspace_name);
-        command.push_str(&focus_command);
-    }
-
-    i3.run_command(command)
-        .map_err(|_| "Unable to execute the command")?;
+    let free_workspace_name = find_next_free_workspace(&opt.names, &active_workspaces)?;
+    i3.run_command(build_command(&opt, &free_workspace_name))
+        .map_err(|_| "Unable to execute the command.")?;
 
     Ok(())
 }
@@ -76,38 +64,59 @@ fn is_action_necessary(
         return Err("No empty workspaces available.");
     }
 
-    let focused_workspace_name = Some(
-        active_workspaces
-            .iter()
-            .filter(|w| w.focused)
-            .next()
-            .expect("There is always one focused workspace")
-            .name
-            .clone(),
+    let focused_workspace_name = active_workspaces
+        .iter()
+        .filter(|w| w.focused)
+        .next()
+        .map(|w| w.name.clone());
+    assert!(
+        focused_workspace_name.is_some(),
+        "There must always be one focused workspace."
     );
 
     let i3_node_name = Some(String::from("__i3"));
     let content_node_name = Some(String::from("content"));
 
-    let tree: Node = i3.get_tree().map_err(|_| "Unable to access tree.")?;
+    let tree: Node = i3.get_tree().expect("Unable to access tree.");
+    // Retrieve nodes for workspaces from tree and check if the current workspace is empty, i.e., has no nodes.
     if tree
         .nodes
         .iter()
-        .filter(|o| !o.name.eq(&i3_node_name))
-        .flat_map(|o| &o.nodes)
-        .filter(|c| c.name.eq(&content_node_name))
-        .flat_map(|c| &c.nodes)
+        .filter(|o| !o.name.eq(&i3_node_name)) // Output nodes only.
+        .flat_map(|o| &o.nodes) // Output node children.
+        .filter(|c| c.name.eq(&content_node_name)) // Each output node has a content node.
+        .flat_map(|c| &c.nodes) // Content node children are workspaces.
         .any(|w: &Node| w.name.eq(&focused_workspace_name) && w.nodes.len() == 0)
     {
         Err("The current workspace is empty.")
     } else {
+        // Action is necessary. Return Ok.
         Ok(())
     }
 }
 
-fn find_next_free_workspace(all_names: &Vec<String>, active_names: &Vec<String>) -> Option<String> {
+fn find_next_free_workspace(
+    all_names: &Vec<String>,
+    active_workspaces: &Vec<Workspace>,
+) -> Result<String, &'static str> {
+    let active_names: Vec<&String> = active_workspaces.iter().map(|ws| &ws.name).collect();
     all_names
         .iter()
-        .find(|&name| !active_names.contains(name))
+        .find(|name| !active_names.contains(name))
         .cloned()
+        .ok_or("No empty workspaces available.")
+}
+
+fn build_command(opt: &Opt, workspace_name: &String) -> String {
+    let mut command = String::new();
+
+    if opt.move_container {
+        command.push_str(&format!("move container to workspace {0};", workspace_name));
+    }
+
+    if !opt.stay {
+        command.push_str(&format!("workspace {0};", workspace_name));
+    }
+
+    command
 }
